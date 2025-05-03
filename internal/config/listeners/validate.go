8e1fd2fee16ecdbd8743fbc35b6f26dc454b7ab7
RT <robbyt@robbyt.net>
2025-05-03 13:34:28 -0400

    Add validation interface to listener options

--- internal/config/listeners/validate.go
+++ internal/config/listeners/validate.go
@@ -11,19 +11,18 @@ import (
 func (l *Listener) Validate() error {
 	var errs []error
 
-	// Validate ID
 	if l.ID == "" {
 		errs = append(errs, fmt.Errorf("%w: listener ID", errz.ErrEmptyID))
 	}
 
-	// Validate Address
 	if l.Address == "" {
 		errs = append(errs, fmt.Errorf("%w: address for listener '%s'",
 			errz.ErrMissingRequiredField, l.ID))
 	}
 
 	// Validate Type
-	switch l.GetType() {
+	listenerType := l.GetType()
+	switch listenerType {
 	case TypeHTTP, TypeGRPC:
 		// Valid types
 	case "":
@@ -31,49 +30,57 @@ func (l *Listener) Validate() error {
 			errz.ErrMissingRequiredField, l.ID))
 	default:
 		errs = append(errs, fmt.Errorf("%w: listener '%s' has invalid type '%s'",
-			errz.ErrInvalidListenerType, l.ID, l.GetType()))
+			errz.ErrInvalidListenerType, l.ID, listenerType))
 	}
 
-	// Validate Options
-	if l.Options != nil {
-		if l.Options.Type() != l.GetType() {
-			errs = append(errs, fmt.Errorf(
-				"mismatch between listener type '%s' and options type '%s' for listener '%s'",
-				l.GetType(), l.Options.Type(), l.ID))
+	// Handle nil Options case
+	if l.Options == nil {
+		if listenerType != "" {
+			errs = append(errs, fmt.Errorf("%w: listener '%s' has type '%s' but no options",
+				errz.ErrMissingRequiredField, l.ID, listenerType))
 		}
+		return errors.Join(errs...)
+	}
 
-		// Type-specific validations
-		switch opts := l.Options.(type) {
-		case HTTPOptions:
-			// Validate HTTP-specific options
-			if l.GetType() != TypeHTTP {
-				errs = append(errs, fmt.Errorf(
-					"listener '%s' has HTTP options but type is '%s'",
-					l.ID, l.GetType()))
-			}
-
-			// Additional HTTP option validations could go here
-
-		case GRPCOptions:
-			// Validate gRPC-specific options
-			if l.GetType() != TypeGRPC {
-				errs = append(errs, fmt.Errorf(
-					"listener '%s' has gRPC options but type is '%s'",
-					l.ID, l.GetType()))
-			}
-
-			// Additional gRPC option validations could go here
-
-		default:
-			errs = append(errs, fmt.Errorf(
-				"%w: listener '%s' has unknown options type %T",
-				errz.ErrInvalidListenerType, l.ID, opts))
-		}
-	} else if l.GetType() != "" {
-		// Options are optional, but if the type is set, we should have matching options
+	optionsType := l.Options.Type()
+	if optionsType != listenerType {
 		errs = append(errs, fmt.Errorf(
-			"%w: listener '%s' has type '%s' but no options",
-			errz.ErrMissingRequiredField, l.ID, l.GetType()))
+			"mismatch between listener type '%s' and options type '%s' for listener '%s'",
+			listenerType, optionsType, l.ID))
+	}
+
+	// Type-specific validations
+	switch opts := l.Options.(type) {
+	case HTTPOptions:
+		if listenerType != TypeHTTP {
+			errs = append(errs, fmt.Errorf(
+				"listener '%s' has HTTP options but type is '%s'",
+				l.ID, listenerType))
+		}
+		
+		// Validate HTTP-specific options
+		if optErr := opts.Validate(); optErr != nil {
+			errs = append(errs, fmt.Errorf("invalid HTTP options for listener '%s': %w", 
+				l.ID, optErr))
+		}
+
+	case GRPCOptions:
+		if listenerType != TypeGRPC {
+			errs = append(errs, fmt.Errorf(
+				"listener '%s' has gRPC options but type is '%s'",
+				l.ID, listenerType))
+		}
+		
+		// Validate gRPC-specific options
+		if optErr := opts.Validate(); optErr != nil {
+			errs = append(errs, fmt.Errorf("invalid gRPC options for listener '%s': %w", 
+				l.ID, optErr))
+		}
+
+	default:
+		errs = append(errs, fmt.Errorf(
+			"%w: listener '%s' has unknown options type %T",
+			errz.ErrInvalidListenerType, l.ID, opts))
 	}
 
 	return errors.Join(errs...)
