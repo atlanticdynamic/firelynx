e78b539da736ba4f5e260388229f51e4db882bf2
RT <robbyt@robbyt.net>
2025-05-26 00:19:17 -0400

    relocate logging helper

--- cmd/firelynx/logger.go
+++ cmd/firelynx/logger.go
@@ -1,31 +1,10 @@
 package main
 
 import (
-	"log/slog"
-	"os"
-	"strings"
+	"github.com/atlanticdynamic/firelynx/internal/logging"
 )
 
 // SetupLogger configures the default logger based on provided log level
 func SetupLogger(logLevel string) {
-	level := slog.LevelInfo // Default level
-
-	// Parse log level
-	switch strings.ToLower(logLevel) {
-	case "debug":
-		level = slog.LevelDebug
-	case "info":
-		level = slog.LevelInfo
-	case "warn", "warning":
-		level = slog.LevelWarn
-	case "error":
-		level = slog.LevelError
-	}
-
-	// Configure and set the default logger
-	handler := slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
-		Level: level,
-	})
-
-	slog.SetDefault(slog.New(handler))
+	logging.SetupLogger(logLevel)
 }
