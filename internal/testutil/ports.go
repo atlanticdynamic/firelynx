cb535749ab351a4cc096fa3dab9e2af143a83ba9
RT <robbyt@robbyt.net>
2025-05-29 17:44:42 -0400

    adjust mutex on test helper

--- internal/testutil/ports.go
+++ internal/testutil/ports.go
@@ -7,38 +7,37 @@ import (
 	"testing"
 )
 
-// reduce the chance of port conflicts
 var (
 	portMutex = &sync.Mutex{}
 	usedPorts = make(map[int]struct{})
 )
 
-// GetRandomPort finds an available port for the test by binding to port 0
 func GetRandomPort(t *testing.T) int {
 	t.Helper()
 	portMutex.Lock()
-	defer portMutex.Unlock()
 	listener, err := net.Listen("tcp", ":0")
 	if err != nil {
+		portMutex.Unlock()
 		t.Fatalf("Failed to get random port: %v", err)
 	}
 
 	err = listener.Close()
 	if err != nil {
+		portMutex.Unlock()
 		t.Fatalf("Failed to close listener: %v", err)
 	}
 
 	addr := listener.Addr().(*net.TCPAddr)
 	p := addr.Port
-	// Check if the port is already used
 	if _, ok := usedPorts[p]; ok {
+		portMutex.Unlock()
 		return GetRandomPort(t)
 	}
 	usedPorts[p] = struct{}{}
+	portMutex.Unlock()
 	return p
 }
 
-// GetRandomListeningPort finds an available port for the test by binding to port 0
 func GetRandomListeningPort(t *testing.T) string {
 	t.Helper()
 	p := GetRandomPort(t)
