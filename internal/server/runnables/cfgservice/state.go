73fc23899baf7cf469ac9fc3fb0879977c1e727f
RT <robbyt@robbyt.net>
2025-05-29 16:18:38 -0400

    switch the cfgservice to use the new go-fsm WithSyncBroadcast() feature

--- internal/server/runnables/cfgservice/state.go
+++ internal/server/runnables/cfgservice/state.go
@@ -18,5 +18,5 @@ func (r *Runner) GetState() string {
 
 // GetStateChan returns a channel that emits the runner's state whenever it changes
 func (r *Runner) GetStateChan(ctx context.Context) <-chan string {
-	return r.fsm.GetStateChan(ctx)
+	return r.fsm.GetStateChanWithOptions(ctx, finitestate.WithSyncBroadcast())
 }
